399bbf3a23a50060f96edd18fe5519c402ac28e2
abeidahmed <abeidahmed92@gmail.com>
2021-08-19 11:50:16 +0530

    extend from ApplicationController class

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,3 +1,11 @@
 const { environment } = require('@rails/webpacker')
+const webpack = require('webpack')
+
+environment.plugins.append(
+  'Provide',
+  new webpack.ProvidePlugin({
+    ApplicationController: ['application_controller', 'default'],
+  })
+)
 
 module.exports = environment
